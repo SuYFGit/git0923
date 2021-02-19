83282116b54f21266c8a01d062d93ad02648a19b
SuYF <SuYF@atguigu.com>
2021-02-19 19:25:12 +0800

    third commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,5 +6,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("版本2");
+        System.out.println("hot-fix");
     }
 }
